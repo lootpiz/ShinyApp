18364b9310ba1c8516f2c826fdf9b348029dec8a
Heewon Seo <lootpiz@gmail.com>
2022-07-21 08:09:14 -0600

    Updated: cptac tfri datasets added

--- app.R
+++ app.R
@@ -1,17 +1,39 @@
 library(ggplot2)
 library(shiny)
 
-data <- readRDS(file.path("./data", "TCGA-GBM_Expression_DESEQ2.RDS"))
-gtex <- readRDS(file.path("./data", "GTEx-Cortex_Expression_DESEQ2.RDS"))
-nmfr <- readRDS(file.path("./data", "TCGA_NMFres.RDS"))
+tcga_data <- readRDS(file.path("./data", "TCGA_Expression.RDS"))
+tcga_gtex <- readRDS(file.path("./data", "GTEx_Expression.RDS"))
+tcga_nmfr <- readRDS(file.path("./data", "TCGA_NMF.RDS"))
+tcga_rank <- 5
 
-rank <- 5
+cptac_data <- log2(readRDS(file.path("./data", "CPTAC_Expression.RDS")))
+cptac_gtex <- cptac_data[,c(100:108)]
+cptac_data <- cptac_data[,c(1:99)]
+cptac_nmfr <- readRDS(file.path("./data", "CPTAC_NMF.RDS"))
+cptac_rank <- 4
+
+tfri_data <- readRDS(file.path("./data", "TFRI_Expression.RDS"))
+tfri_gtex <- NULL
+tfri_nmfr <- readRDS(file.path("./data", "TFRI_NMF.RDS"))
+tfri_rank <- 3
+
+colours <- c("gold", "orange", "brown1", "brown4", "indianred3")
 
 ui <- fluidPage(
-        titlePanel("TCGA-GBM Bulk RNA-seq"),
+        titlePanel("GBM Bulk RNA-seq"),
         sidebarLayout(
                 sidebarPanel(
-                        helpText("A combined cohort of TCGA-GBM and GTEx samples downloaded from Xenabrowser. Gene expression profile is RSEM expected_count output normalized using DESeq2."),
+                        helpText("Query the level of gene expression in either TCGA, CPTAC, or TFRI."),
+                        radioButtons(
+                                inputId = "dataset", 
+                                label = h3("Dataset"),
+                                choices = list(
+                                        "TCGA" = 1, 
+                                        "CPTAC" = 2,
+                                        "TFRI" = 3
+                                ),
+                                selected = 1
+                        ),
                         textInput(
                                 inputId = "gene",
                                 label = h3("Gene symbol"),
@@ -20,14 +42,15 @@ ui <- fluidPage(
                         downloadLink(
                                 outputId = "export",
                                 label = "Export Figure(s)"
-                        )       
+                        )
                 ),
                 mainPanel(
-                        h2("TCGA-GBM, N=164 vs. GTEx-Cortex, N=105"),
+                        h2(textOutput("selected_dataset")),
+                        h5(textOutput("help_dataset")),
                         tabsetPanel(
                                 type = "tabs",
                                 tabPanel("NMF subgroups", h5("Subgroups were defined via NMF (Non-negatvie Matrix Factorization)"), plotOutput("violin")),
-                                tabPanel("GBM vs. Brain", plotOutput("violin2"))
+                                tabPanel("Tumour vs. Normal", plotOutput("violin2"))
                         )                        
                 )
         )
@@ -35,14 +58,36 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
         generateViolinNMF <- function() {
-                gene <- input$gene
-                if (length(which(rownames(gtex) == gene)) > 0) {
-                        results <- data.frame(
-                                Group = "GTEx",
-                                Expression = as.numeric(gtex[which(rownames(gtex) == gene), ])
-                        )
+                if (input$dataset == 1) {
+                        data <- tcga_data
+                        gtex <- tcga_gtex
+                        nmfr <- tcga_nmfr
+                        rank <- tcga_rank
+                } else if (input$dataset == 2) {
+                        data <- cptac_data
+                        gtex <- cptac_gtex
+                        nmfr <- cptac_nmfr
+                        rank <- cptac_rank
+                } else if (input$dataset == 3) {
+                        data <- tfri_data
+                        gtex <- tfri_gtex
+                        nmfr <- tfri_nmfr
+                        rank <- tfri_rank
+                }
 
-                        subgroups <- nmfr 
+                gene <- input$gene
+
+                if (length(which(rownames(data) == gene)) > 0) {
+                        if (!is.null(gtex)) {
+                                results <- data.frame(
+                                        Group = "GTEx",
+                                        Expression = as.numeric(gtex[which(rownames(gtex) == gene), ])
+                                )
+                        } else {
+                                results <- c()
+                        }
+
+                        subgroups <- nmfr
 
                         for (subGroupLv in c(1:rank)) {
                                 dataSubIdx <- which(colnames(data) %in% names(subgroups)[which(subgroups == subGroupLv)])
@@ -55,16 +100,24 @@ server <- function(input, output, session) {
                                 results <- rbind(results, df)
                         }
 
-                        results$Group <- factor(results$Group, levels = c(paste0("Group_", c(1:rank)), "GTEx"))
+                        if (!is.null(gtex)) {
+                                lev <- c(paste0("Group_", c(1:rank)), "GTEx")
+                                col <- c(colours[1:rank], "grey80")
+                        } else {
+                                lev <- c(paste0("Group_", c(1:rank)))
+                                col <- c(colours[1:rank])
+                        }
+
+                        results$Group <- factor(results$Group, levels = lev)
 
                         dodge <- position_dodge(width = 0.5)
                         ggplot(data = results, aes(x = Group, y = Expression, fill = Group)) +
                                 geom_violin(position = dodge, size = 0) +
                                 geom_boxplot(width = 0.1, position = dodge, fill = "white") +
-                                scale_fill_manual(values = c("gold", "orange", "brown1", "brown4", "indianred3", "grey80")) +
+                                scale_fill_manual(values = col) +
                                 labs(
                                         title = gene,
-                                        x = "", y = "Expression, Log2"
+                                        x = "", y = "Expression"
                                 ) +
                                 theme_bw() +
                                 theme(
@@ -80,26 +133,52 @@ server <- function(input, output, session) {
                 }
         }
         generateViolinTvsB <- function() {
-                gene <- input$gene
-                if (length(which(rownames(gtex) == gene)) > 0) {
-                        resultsGtex <- data.frame(
-                                Group = "GTEx-Cortex",
-                                Expression = as.numeric(gtex[which(rownames(gtex) == gene), ])
-                        )
+                if (input$dataset == 1) {
+                        data <- tcga_data
+                        gtex <- tcga_gtex
+                        nmfr <- tcga_nmfr
+                        rank <- tcga_rank
+                } else if (input$dataset == 2) {
+                        data <- cptac_data
+                        gtex <- cptac_gtex
+                        nmfr <- cptac_nmfr
+                        rank <- cptac_rank
+                } else if (input$dataset == 3) {
+                        data <- tfri_data
+                        gtex <- tfri_gtex
+                        nmfr <- tfri_nmfr
+                        rank <- tfri_rank
+                }
 
+                gene <- input$gene
+
+                if (length(which(rownames(data) == gene)) > 0) {
                         resultsGbm <- data.frame(
-                                Group = "TCGA-GBM",
+                                Group = "GBM",
                                 Expression = as.numeric(data[which(rownames(data) == gene), ])
                         )
 
-                        results <- rbind(resultsGbm, resultsGtex)
-                        results$Group <- factor(results$Group, levels = c("TCGA-GBM", "GTEx-Cortex"))
+                        if (!is.null(gtex)) {        
+                                resultsGtex <- data.frame(
+                                        Group = "Brain",
+                                        Expression = as.numeric(gtex[which(rownames(gtex) == gene), ])
+                                )
+                                results <- rbind(resultsGbm, resultsGtex)
+                                lev <- c("GBM", "Brain")
+                                col <- c("chartreuse2", "grey80")
+                        } else {
+                                results <- resultsGbm
+                                lev <- c("GBM")
+                                col <- c("chartreuse2")
+                        }
+
+                        results$Group <- factor(results$Group, levels = lev)
 
                         dodge <- position_dodge(width = 0.5)
                         ggplot(data = results, aes(x = Group, y = Expression, fill = Group)) +
                                 geom_violin(position = dodge, size = 0) +
                                 geom_boxplot(width = 0.1, position = dodge, fill = "white") +
-                                scale_fill_manual(values = c("chartreuse2", "grey80")) +
+                                scale_fill_manual(values = col) +
                                 labs(
                                         title = gene,
                                         x = "", y = "Expression, Log2"
@@ -123,7 +202,16 @@ server <- function(input, output, session) {
 
         output$export <- downloadHandler(
                 filename = function() {
-                        paste0("TCGA_", input$gene, "_", Sys.Date(), ".pdf")
+                        if (input$dataset == 1) {
+                                dName <- "TCGA"
+                        } else if (input$dataset == 2) {
+                                dName <- "CPTAC"
+                        } else if (input$dataset == 3) {
+                                dName <- "TFRI"
+                        } else {
+                                dName <- "Wrong"
+                        }
+                        paste0(dName, "_", input$gene, "_", Sys.Date(), ".pdf")
                 },
                 content = function(file) {
                         pdf(file = file)
@@ -132,6 +220,30 @@ server <- function(input, output, session) {
                         dev.off()
                 }
         )
+
+        output$selected_dataset <- renderText({
+                if (input$dataset == 1) {
+                        "TCGA-GBM, N=164 vs. GTEx-Cortex, N=105"
+                } else if (input$dataset == 2) {
+                        "CPTAC-GBM, N=99 vs. GTEx-Brain, N=10"
+                } else if (input$dataset == 3) {
+                        "TFRI-Tumor, N=44"
+                } else {
+                        "Something went wrong..."
+                }
+        })
+
+        output$help_dataset <- renderText({
+                if (input$dataset == 1) {
+                        "A combined cohort of TCGA and GTEx samples downloaded from Xenabrowser. Gene expression profile is RSEM expected_count output normalized using DESeq2."
+                } else if (input$dataset == 2) {
+                        "Prospectively collected tumor tissue samples from 99 GBM patients and 10 normal brain samples from CPTAC and GTEx, respectively."
+                } else if (input$dataset == 3) {
+                        "No (normal) brain profile available. Shen, Grisdale, Islam et al. PNAS (2019). DOI:10.1073/pnas.1813495116"
+                } else {
+                        "Something went wrong..."
+                }
+        })
 }
 
 shinyApp(ui = ui, server = server)
